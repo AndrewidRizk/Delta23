372414140b2eab85bd357d468a0bf0e33b297d66
andrewlgoetz <100867543+andrewlgoetz@users.noreply.github.com>
2023-01-14 16:00:58 -0500

    ready for flask

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template, request, url_for, redirect
 import string
 import os, sys
 import json
-#userdata = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'userdata.json')
-userdata = "userdata.json"
+
+
+
+userdata = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'userdata.json')
+#userdata = "userdata.json"
+
+
+
+def driver(username, new_movie):
+    toPost = [[],[]]
 
 
 def getAuth(username):
@@ -12,9 +20,20 @@ def getAuth(username):
     try:
         with open(userdata, 'r') as fh:
             data = json.load(fh)
+            user_exists = False
             for user in data['users']:
                 if user['id'] == username:
+                    user_exists = True
                     movies = user['movies']
+                    break
+            if not user_exists:
+                data['users'].append({'id': username, 'movies': []})
+                with open(userdata, 'w') as jsonfile:
+                    json.dump(data, jsonfile, indent=4)
+                
+
+
+
     except FileNotFoundError:
         print("The file userdata.json was not found.")
     except json.decoder.JSONDecodeError:
@@ -33,13 +52,14 @@ def postMovie(user, movie):
             user_exists = True
             user['movies'].append(movie)
             break
-    if not user_exists:
-        data['users'].append({'id': user, 'movies': [movie]})
+        ## User doesnt exists handled in getAuth
+    # if not user_exists:
+    #     data['users'].append({'id': user, 'movies': [movie]})
     
     with open(userdata, 'w') as fh:
-        json.dump(data, fh)
+        json.dump(data, fh, indent=4)
 
 
 
-user_id = 'example_user'
-print(getAuth(user_id))
+# user_id = 'example_user'
+# print(getAuth(user_id))
